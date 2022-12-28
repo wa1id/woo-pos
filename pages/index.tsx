d2a586dc758425faa233bd72f500f068ae95326a
Wa1iD <wyachou95@gmail.com>
2022-12-28 15:50:40 +0100

    :sparkles: show products by category

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import * as Accordion from "@radix-ui/react-accordion";
 import axios from "axios";
 import { formatPrice } from "../utils";
 
-export default function Home({ products }: { products: Product[] }) {
+interface Props {
+  products: Product[];
+  categories: Category[];
+}
+
+export default function Home({ products, categories }: Props) {
   return (
     <>
-      <h1 className="underline">Producten</h1>
+      <div className="underline font-bold">Producten</div>
       {products.map((product: Product) => (
         <Accordion.Root key={product.id} type="multiple">
           <Accordion.Item value={product.slug}>
@@ -18,23 +23,60 @@ export default function Home({ products }: { products: Product[] }) {
           </Accordion.Item>
         </Accordion.Root>
       ))}
+
+      <div className="underline font-bold">Categories</div>
+      {categories.map((category: Category) => (
+        <Accordion.Root key={category.id} type="multiple">
+          <Accordion.Item value={category.slug}>
+            <Accordion.Header>
+              <Accordion.Trigger>{category.name}</Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content>
+              <>
+                {products
+                  .filter((product) =>
+                    product.categories.some((cat) => cat.id === category.id)
+                  )
+                  .map((product) => (
+                    <div key={product.id}>{product.name}</div>
+                  ))}
+              </>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion.Root>
+      ))}
     </>
   );
 }
 
 export async function getStaticProps() {
-  const res = axios.get(`${process.env.WOO_URL}/wp-json/wc/v3/products`, {
-    auth: {
-      username: process.env.WOO_CONSUMER_KEY as string,
-      password: process.env.WOO_CONSUMER_SECRET as string,
-    },
-  });
+  const res = axios.get(
+    `${process.env.WOO_URL}/wp-json/wc/v3/products?per_page=100`,
+    {
+      auth: {
+        username: process.env.WOO_CONSUMER_KEY as string,
+        password: process.env.WOO_CONSUMER_SECRET as string,
+      },
+    }
+  );
+
+  const resCategories = axios.get(
+    `${process.env.WOO_URL}/wp-json/wc/v3/products/categories?per_page=100`,
+    {
+      auth: {
+        username: process.env.WOO_CONSUMER_KEY as string,
+        password: process.env.WOO_CONSUMER_SECRET as string,
+      },
+    }
+  );
 
   const products: Product[] = (await res).data || [];
+  const categories: Category[] = (await resCategories).data || [];
 
   return {
     props: {
       products,
+      categories,
     },
     revalidate: 60,
   };
--- typings.d.ts
+++ typings.d.ts
@@ -3,4 +3,11 @@ interface Product {
   name: string;
   slug: string;
   price: string;
+  categories: Category[];
+}
+
+interface Category {
+  id: number;
+  name: string;
+  slug: string;
 }
